da16cd168f21cfba03db862bb2d1c737b4315213
huttsMichael <huttsMichael@gmail.com>
2021-06-13 13:59:54 -0400

    many improvements
    reworked methods for better object orientation.
    reduced calls to getDividends drastically

--- optimalDividends.py
+++ optimalDividends.py
@@ -1,13 +1,33 @@
 import requests
 import json
 import time
+from datetime import datetime, timedelta
+
+WAIT_TIME = 15 # seconds
 
 class Dividends():
     def __init__(self, file = "dividends.json") -> None:
         self.filename = file
         self.histDividends = self.load()
-        self.currentDividends, self.currentPrices = self.get() # getDivs should check if divs are old
+        self.updateDividends = self.__renew()
+        self.currentDividends, self.currentPrices = self.get()
         
+    def __renew(self):
+        for ts in self.histDividends:
+            timestamp = int(ts)
+            if timestamp > 10000000000: 
+                # timestamps had milliseconds added, divide those out if needed
+                timestamp /= 1000
+            lastTime = datetime.utcfromtimestamp(timestamp)
+        
+        diffTime = datetime.now() - lastTime
+        print(diffTime.days)
+
+        if diffTime.days < 7: # no need to call getDividends if 
+            return False
+        else:
+            return True
+    
     def load(self):
         with open("dividends.json") as divs:
             hist = json.load(divs)
@@ -30,20 +50,35 @@ class Dividends():
         else:
             print("no new data\n")
             
-    def get(self, average = False):
+    def get(self):
         print("Get dividends")
-        rawDividends = requests.get("https://nasfaq.biz/api/getDividends")
-        if not average:
+        
+        if self.updateDividends:
+            print("There should be new dividends!")
+            rawDividends = getURL("https://nasfaq.biz/api/getDividends")
             dividends = rawDividends.json()['dividends']['payouts']
+            self.log(rawDividends)
         else:
-            dividends = {}
-        self.log(rawDividends)
+            # use old dividend data if it hasn't been updated
+            for ts in self.histDividends:
+                dividends = self.histDividends[ts]
 
         print("Get prices")
-        rawPrices = requests.get("https://nasfaq.biz/api/getMarketInfo")
+        rawPrices = getURL("https://nasfaq.biz/api/getMarketInfo")
         prices = rawPrices.json()['coinInfo']['data']
 
         return dividends, prices
+
+def getURL(url):
+    success = False
+    while not success:
+        try:
+            resp = requests.get(url)
+            if resp.status_code == requests.codes.ok:
+                return resp
+        except:
+            time.sleep(WAIT_TIME)
+
 
 def calculateRatios(dividends, prices):
     print("\nCalculating ratios")
@@ -61,17 +96,9 @@ def sortRatios(unsortedRatios, descending=False):
     return dict(sorted(unsortedRatios.items(), key=lambda item: item[1], reverse=descending))
 
 if __name__ == '__main__':
-    failed = True
-    while failed:
-        try:
-            divs = Dividends()
-            failed = False
-        except:
-            print("Failed... ", end='')
-            time.sleep(15)
-            print("Retrying")
-            continue
-    
+    divs = Dividends()
+    failed = False
+
     calc = calculateRatios(divs.currentDividends, divs.currentPrices)
     ratios = sortRatios(calc, descending=True)
     for holo in ratios:
