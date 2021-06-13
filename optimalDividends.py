49f17aff6205e89c808d8933ec460d261f16e10b
huttsMichael <huttsMichael@gmail.com>
2021-06-13 11:39:01 -0400

    moving optimalDividends to class-based structure

--- optimalDividends.py
+++ optimalDividends.py
@@ -2,44 +2,48 @@ import requests
 import json
 import time
 
-def loadDividends():
-    with open("dividends.json") as divs:
-        histDividends = json.load(divs)
-    return histDividends
-
-def logDividends(rawDividends):
-    print("\nLog dividends")
-    histDividends = loadDividends()
-
-    newDate = True
-    for timestamp in histDividends:
-        if str(rawDividends.json()['dividends']['timestamp']) == timestamp:
-            newDate = False
-        else:
-            print(str(rawDividends.json()['dividends']['timestamp']), "and", timestamp, "are not equal")
-
-    if newDate:
-        print("new date!", timestamp, "\n")
-        histDividends[str(rawDividends.json()['dividends']['timestamp'])] = rawDividends.json()['dividends']['payouts']
-        with open("dividends.json", 'w') as divs:
-            json.dump(histDividends, divs)
-    else:
-        print("no new data\n")
+class Dividends():
+    def __init__(self, file = "dividends.json") -> None:
+        self.filename = file
+        self.histDividends = self.load()
+        self.currentDividends, self.currentPrices = self.get() # getDivs should check if divs are old
         
-def getDividends(average = False):
-    print("Get dividends")
-    rawDividends = requests.get("https://nasfaq.biz/api/getDividends")
-    if not average:
-        dividends = rawDividends.json()['dividends']['payouts']
-    else:
-        dividends = {}
-    logDividends(rawDividends)
+    def load(self):
+        with open("dividends.json") as divs:
+            hist = json.load(divs)
+        return hist
 
-    print("Get prices")
-    rawPrices = requests.get("https://nasfaq.biz/api/getMarketInfo")
-    prices = rawPrices.json()['coinInfo']['data']
+    def log(self, rawDividends):
+        print("\nLogging dividends")
+        newDate = True
+        for timestamp in self.histDividends:
+            if str(rawDividends.json()['dividends']['timestamp']) == timestamp:
+                newDate = False
+            else:
+                print(str(rawDividends.json()['dividends']['timestamp']), "and", timestamp, "are not equal")
 
-    return dividends, prices
+        if newDate:
+            print("new date!", timestamp, "\n")
+            self.histDividends[str(rawDividends.json()['dividends']['timestamp'])] = rawDividends.json()['dividends']['payouts']
+            with open("dividends.json", 'w') as divs:
+                json.dump(self.histDividends, divs)
+        else:
+            print("no new data\n")
+            
+    def get(self, average = False):
+        print("Get dividends")
+        rawDividends = requests.get("https://nasfaq.biz/api/getDividends")
+        if not average:
+            dividends = rawDividends.json()['dividends']['payouts']
+        else:
+            dividends = {}
+        self.log(rawDividends)
+
+        print("Get prices")
+        rawPrices = requests.get("https://nasfaq.biz/api/getMarketInfo")
+        prices = rawPrices.json()['coinInfo']['data']
+
+        return dividends, prices
 
 def calculateRatios(dividends, prices):
     print("\nCalculating ratios")
@@ -60,24 +64,17 @@ if __name__ == '__main__':
     failed = True
     while failed:
         try:
-            div, price = getDividends()
-            failed = False
-        except:
-            time.sleep(15)
-            continue
-
-    failed = True
-    while failed:
-        try:
-            calc = calculateRatios(div, price)
-            ratios = sortRatios(calc, descending=True)
-            for holo in ratios:
-                print(holo + ": " + "%.3f" % ratios[holo])
+            divs = Dividends()
             failed = False
         except:
             print("Failed... ", end='')
             time.sleep(15)
             print("Retrying")
             continue
+    
+    calc = calculateRatios(divs.currentDividends, divs.currentPrices)
+    ratios = sortRatios(calc, descending=True)
+    for holo in ratios:
+        print(holo + ": " + "%.3f" % ratios[holo])
     
     
