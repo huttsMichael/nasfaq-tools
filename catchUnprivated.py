698bc005133b0eeb9319099e267e30a95ea0b0d1
huttsMichael <huttsMichael@gmail.com>
2021-06-15 10:42:44 -0400

    hopefully fixed narrowed down alertable behavior

--- catchUnprivated.py
+++ catchUnprivated.py
@@ -7,6 +7,7 @@ class Channels:
     def __init__(self) -> None:
         self.data = self.get()
         self.diffs = self.analyze()
+        self.uploads = self.count()
 
     def get(self):
         dataA = getURL("https://api.holotools.app/v1/channels?offset=0&limit=50")
@@ -17,6 +18,14 @@ class Channels:
             dataC.append(channel)
         
         return(dataC)
+
+    def count(self):
+        videos = []
+
+        for channel in self.data:
+            videos.append(channel['video_original'])
+
+        return videos
     
     def analyze(self):
         diffs = []
@@ -29,25 +38,29 @@ class Channels:
     def check(self):
         self.data = self.get()
         diffs = self.analyze()
+        uploads = self.count()
 
         videoFound = False
         for n in range(len(diffs)):
-                if diffs[n] != self.diffs[n]:
+                if diffs[n] != self.diffs[n] and uploads[n] == self.uploads[n]:
                     videoFound = True
-                    print("Change found with: ", self.data[n]['name'])
+                    print("Change found with: {}  ---  ({} vs {}, {} vs {})"
+                        .format(self.data[n]['name'], diffs[n], self.diffs[n], uploads[n], self.uploads[n]))
                     if diffs[n] < self.diffs[n]:
                         print("Change type: UNPRIVATED VIDEO")
                     elif diffs[n] > self.diffs[n]:
                         print("Change type: PRIVATED VIDEO")
 
         self.diffs = diffs
+        self.uploads = uploads
         return(videoFound)
 
 
     
 if __name__ == '__main__':
+    print("Initializing...", end=' ')
     ch = Channels()
-    
+    print("Done!")
     while True:
         time.sleep(CHECK_INTERVAL)
         if ch.check():
