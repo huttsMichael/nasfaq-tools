957864569f0602aeff30fbb757769bc6ea4d6c46
huttsMichael <huttsMichael@gmail.com>
2021-07-17 12:36:38 -0400

    opt-in auto-backup on failure

--- logLeaderboard.py
+++ logLeaderboard.py
@@ -4,7 +4,8 @@ import time
 import sys
 
 MAX_RETRIES = 10
-SLEEP_TIME = 15 # seconds
+SLEEP_TIME = 30 # seconds
+AUTO_BACKUP = False
 DEBUG = True
 
 def getLeaderboard():
@@ -46,7 +47,7 @@ if __name__ == "__main__":
             print(time.strftime("%H:%M:%S", time.localtime()), "retrieving leaderboard")
             getLeaderboard()
             print(time.strftime("%H:%M:%S", time.localtime()), "retrieved leaderboard")
-            time.sleep(600)
+            time.sleep(900)
             retries = MAX_RETRIES # reset retry counter if it runs succesfully
         except KeyboardInterrupt:
             print("\nfixing")
@@ -64,9 +65,10 @@ if __name__ == "__main__":
             continue
         except:
             print("Other Failure:", sys.exc_info()[0])
-            timestr = time.strftime("%Y%m%d-%H%M%S")
-            with open("leaderboard.json", "r") as source, open(timestr + "_leaderboard.json", "w") as dest:
-                dest.write(source.read())
+            if AUTO_BACKUP:
+                timestr = time.strftime("%Y%m%d-%H%M%S")
+                with open("leaderboard.json", "r") as source, open(timestr + "_leaderboard.json", "w") as dest:
+                    dest.write(source.read())
             time.sleep(SLEEP_TIME + (retries * 5))
             retries -= 1
             print("Retries Remaining:", retries)
